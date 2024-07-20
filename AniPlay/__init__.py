4336bf78a6627a2b007715b5e48963b1b1643435
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-07-20 11:34:57 +0530

    Update __init__.py

--- AniPlay/__init__.py
+++ AniPlay/__init__.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Client(
     "AniPlay",
-    api_id= int(os.environ.get("APP_ID", "")),
+    api_id= int(os.environ.get("API_ID", "")),
     api_hash= os.environ.get("API_HASH", ""),
-    bot_token= os.environ.get("TOKEN", "")
+    bot_token= os.environ.get("BOT_TOKEN", "")
 )
